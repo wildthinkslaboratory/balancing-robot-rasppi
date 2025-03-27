178f4b01418a0c9400f1aaa1f26bae04378d76b8
Heidi Dixon <goldfishandrobin@gmail.com>
2025-03-27 13:29:47 -0700

    add position function

--- motors.py
+++ motors.py
@@ -14,13 +14,15 @@ BIN1 = 13
 BIN2 = 19
 STBY = 26
 
-pulses_per_rotation = 11
+count_per_rotation = 310
 wheel_circ = 0.21
+distance_per_count = wheel_circ / count_per_rotation
+
+
 
 class BRMotors:
 	def __init__(self):
 		self.motors = Robot((AIN1, AIN2), (BIN1, BIN2))
-		self.pulses = 0
 		self.pwmA = PWMOutputDevice(PWMA)
 		self.pwmB = PWMOutputDevice(PWMB)
 		self.standby = DigitalOutputDevice(STBY)
@@ -29,12 +31,15 @@ class BRMotors:
 		self.encoder2C1 = Button(ENCODER2_C1)
 		self.encoder2C2 = Button(ENCODER2_C2)
 		self.standby.off()
+		self.encoder1C1.when_pressed = self.inc_counts
+		self.counts = 0
+		self.count_inc = 1
 
-	def encoder1_rising(self):
-		self.pulses +=1
-
-	def get_position(self):
-		return self.pulses / pulses_per_rotation * wheel_circ
+	def inc_counts(self):
+		self.counts += self.count_inc
+		  
+	def position(self):
+		return self.counts * distance_per_count
 
 	def cleanup(self):
 		self.motors.stop()
@@ -45,8 +50,10 @@ class BRMotors:
 	def run(self, speed):
 		self.standby.on()
 		if speed > 0:
+			self.count_inc = 1
 			self.motors.forward()
 		if speed <= 0:
+			self.count_inc = -1
 			self.motors.backward()
 		self.pwmA.value = abs(speed)
 		self.pwmB.value = abs(speed)
@@ -57,8 +64,11 @@ class BRMotors:
 def main():
 	try:
 		my_motors = BRMotors()
-		my_motors.run(0.5)
-		sleep(2)
+		my_motors.run(0.25)
+		sleep(1)
+		my_motors.run(-0.25)
+		sleep(1)
+		print(my_motors.position())
 	finally:
 		my_motors.cleanup()
 
