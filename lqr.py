e013f9baf034c8bb2198147950b6667cb20065cd
wildthinkslaboratory <goldfishandrobin@gmail.com>
2025-03-27 20:13:28 -0700

    fixing up lqr for testing

--- lqr.py
+++ lqr.py
@@ -1,19 +1,18 @@
-import json
 import numpy as np
 from control.matlab import lqr
+from time import sleep
+from motors import BRMotors
+from imu import ImuSensor
 
-
-def distance(pulses):
-    return pulses/pulse_per_rotation * wheel_circ
 
 def generateK():
 
-    m = 0.2        # kilograms
-    M = 0.3        # kilograms
-    L = 0.12     # meters
+    m = 0.29        # kilograms
+    M = 0.765       # kilograms
+    L = 0.14     # meters
     g = -9.81    # meters / sec^2
     d = 0        # d is a damping factor
-
+    
     b = 1 # pendulum up (b=1)
 
     A = np.array([[0,1,0,0],\
@@ -29,16 +28,34 @@ def generateK():
 
 
 
-x0 = np.array([-1,0,np.pi+0.1,0]) # Initial condition
-wr = np.array([1,0,np.pi,0])      # Reference position
-x = x0
+x0 = np.array([0,0,np.pi,0])      # Initial condition
+wr = np.array([0,0,np.pi,0])      # Reference position
 
-K = generateK()
+K = generateK() 
 
-print('K ', K)
-u =  -K@(x-wr)  
-print(u)
+k1 = 1
+k2 = 0
 
-# while True:
-#     # read state from sensors 
-#     pass
+def get_output(state):
+    u = -K@(state - wr)
+    output = k1*u + k2
+    if abs(output) > 100.0:
+        output = 100.0 if output > 0 else -100.0
+    return output
+
+imu_sensor = ImuSensor()
+motors = BRMotors()
+
+try:
+    while True:
+        (a, av) = imu_sensor.angle_data()
+        (x,v) = motors.position_data()
+        state = np.array([x, v, a, av])
+        output = get_output(state)
+
+        motors.run(output / 100)
+        time.sleep(0.01)
+
+
+except KeyboardInterrupt:
+    print("Program Interrupted")
