a899160caf24be3de034f9492bc2af9d2a96c406
wildthinkslaboratory <goldfishandrobin@gmail.com>
2025-06-16 14:46:25 -0700

    still adjusting constants and Q, R matrices

--- model_constants.py
+++ model_constants.py
@@ -5,23 +5,20 @@ g = -9.81       # gravity, (meters / sec^2)
 d = 0.001       # d is a damping factor
 r = 0.0325      # radius of the wheels
 
+motor_stall_torque = 0.5
+max_torque = motor_stall_torque * 0.9
 
 # this is the maximum input u that we allow. 
 # we use this in our R matrix when computing gain K
 # it's a soft constraint really
-# each motor has a maximum torque of 0.5 Nm
-# we want to stay under 80% of that max
+# we want to stay under 80% of the stall torque
 # divide by the radius to get the force in the horizontal direction
 # we have two motors so double it
-max_input_value = ((0.5 * 0.8) / r) * 2.0
-
-print('max input', max_input_value)
-
-max_torque = 0.5 * 0.8
+max_input_value = ((motor_stall_torque * 0.8) / r) * 2.0
 
 # the duty coeffiecent maps a desired torque range (-0.5, 0.5) to a motor
 # speed that is between -1 and 1. 
-duty_coeff = 1 / 0.5
+duty_coeff = 1 / max_torque
 
 # Take the input force u and multiply by radius r
 # to get desired torque. Then we scale it to the motor
