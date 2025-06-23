4c89cd581b7a18ee0c80c56e8c5de5598b786b49
Heidi Dixon <goldfishandrobin@gmail.com>
2025-06-22 17:21:46 -0700

    adding lqr2V

---
+++ lqr2V.py
@@ -0,0 +1,77 @@
+import numpy as np
+from time import sleep
+from InterruptTimer import InterruptTimer
+from motors import BRMotors
+from imu import ImuSensor
+from balance_bot2V import lqrdBot as bb
+from utilities import output_data, clip, countdown
+from builds import ModelConstants
+
+mc = ModelConstants()
+
+debug = False
+output_data_to_file = True
+
+timeout = 8
+dT = bb.dt
+
+imu_sensor = ImuSensor()        # mpu6050 gyro/accelorometer access                    
+motors = BRMotors(dT)           # DC motors with encoder
+
+############################################
+# These are all our loop variables 
+
+############################################
+run_data = list()  
+countdown(5)
+
+angle_init = imu_sensor.raw_angle_rad()
+
+x = np.array([angle_init,0.0])               # this is our estimated state                    
+u = np.array([0.0])          # our input values [ u, x_sensor, a_sensor, av_sensor]   
+
+
+############################################
+# LQE implementation
+# 
+############################################
+
+
+
+
+def update_run_data():
+    global run_data
+    run_data.append([x[0], x[1], u[0]])
+
+def loop_iteration():
+    global x
+    global uy
+
+    x[0] = imu_sensor.raw_angle_rad()  # this needs to be with pi in the up position
+    x[1] = imu_sensor.raw_angular_velocity_rad()
+    
+    # constrain the input to the allowed motor speeds
+    u = bb.get_control_input(x)
+    motor_speed = clip(u[0] / mc.SC, -0.9, 0.9)
+    u[0] = motor_speed * mc.SC    # this is the force our motors can actually apply
+    motors.run(motor_speed)
+
+
+    
+# the main functions are called in timers that
+# keep strict time deltas between calls
+loop_timer = InterruptTimer(dT, loop_iteration, timeout)
+output_timer = InterruptTimer(dT, update_run_data, timeout)
+
+loop_timer.start()
+output_timer.start()
+
+
+# collect runtime data and output it to file
+if output_data_to_file:
+    output_timer.start()
+    while loop_timer.running:
+        sleep(dT)
+    output_data(run_data, "data.json")
+
+
