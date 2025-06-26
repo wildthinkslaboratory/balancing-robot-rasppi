fix variance testing

builds.py
@@ -41,7 +41,7 @@ class ModelConstants:
 class ExperimentalConstants(ModelConstants):
 
     t = 0.1
-    Q2V = np.array([[1,0],\
+    Q2V = np.array([[100,0],\
                     [0, 1]])
 
     R2V = np.array([[1]])
@@ -51,3 +51,22 @@ class ExperimentalConstants(ModelConstants):
     R_kf2V = np.array([[0.001,0],\
                     [0,0.000001]])
     pass
+
+"""Countdown: 1gyro noise variance 2.9101840876873906e-06
+average error gyro error:  0.0013880980414035858
+
+
+testing angular accelleration readings
+position your bot upright
+Countdown: 1
+accelerometer Y variance 0.0010923020283783833
+average accelerometer Y:  0.026022973756104836
+
+
+accelerometer Z variance 0.002279805119738938
+average accelerometer Z:  9.800067777614032
+
+
+accelerometer angle variance 0.037360315197735146
+average angle:  0.15215351475542477
+"""

imu.py
@@ -123,15 +123,14 @@ def verify_accelerometer(imu):
     print("testing angular accelleration readings")
     print('position your bot upright')
     countdown(5)
-    timespan = 2
+    timespan = 5
 
     data = []
-    for _ in range(timespan * 10):
+    for _ in range(timespan * 100):
         imu.raw_accel_data()
         angle = math.degrees(imu.raw_angle_rad() - math.pi) 
-        print('Y:', imu.ay_raw, 'Z:', imu.az_raw, 'angle', angle)
         data.append([imu.ay_raw, imu.az_raw, angle])
-        sleep(0.1)
+        sleep(0.01)
 
     print("\naccelerometer Y variance", np.var([(a[0]) for a in data]))
     print("average accelerometer Y: ", np.average([abs(a[0]) for a in data]))
@@ -159,7 +158,7 @@ def verify_gyro(imu):
     timer.start()
 
     while timer.running: # wait for the trial to end
-        sleep(1)
+        sleep(5)
 
     print("gyro noise variance", np.var([a[0] for a in data]))
     print("average error gyro error: ", np.average([abs(a[0]) for a in data]))
@@ -172,5 +171,7 @@ if __name__ == "__main__":
     import numpy as np  
 
     imu = ImuSensor()
+    print('put bot on block for variance computation')
+    countdown(5)
     verify_gyro(imu)
     verify_accelerometer(imu)