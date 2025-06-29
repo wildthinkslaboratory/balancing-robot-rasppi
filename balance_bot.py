fixing some units issues in variance calculations

balance_bot.py
@@ -33,6 +33,8 @@ d = ca.MX.sym('d')
 ST = ca.MX.sym('ST')
 r = ca.MX.sym('r')
 
+test = ca.MX.sym('p', 3)
+print(test.size1())
 
 constants = ca.vertcat( M, m, L, g, d, ST, r )
 
@@ -79,30 +81,28 @@ C = np.array([[1, 0, 0, 0], \
 lqgdBot.set_up_K(pmc.Q, pmc.R, goal_state, goal_u)
 lqgdBot.set_up_kalman_filter(C, pmc.Q_kf, pmc.R_kf)
 
-
 print(lqgdBot)
-
 if __name__ == "__main__":
     # now we can rum a simulation
     u0 = np.array([0.0])
-    x0 = np.array([1.0,0,np.pi, 0.0]) # Initial condition
-    sim_length = 10 # in seconds
+    x0 = np.array([0.0,0,np.pi, 0.0]) # Initial condition
+    sim_length = 1 # in seconds
 
-    simulator = Simulator(lqgdBot, x0, u0, sim_length)
+    # simulator = Simulator(lqgdBot, x0, u0, sim_length)
+    # simulator.run()
+
+    variances = np.array([0.000004, 0.01, 0.000003])
+    simulator = NoisySimulator(lqgdBot, x0, u0, sim_length, noise=variances, nudge=0.0)
     simulator.run()
 
-    variances = np.array([0.001, 0.05, 0.000003])
-    simulator = NoisySimulator(lqgdBot, x0, u0, sim_length, noise=variances, nudge = 0.5)
-    simulator.run()
+    # run_data = import_data('data.json')
+    # num_cols = len(run_data[0])
+    # sensor_data = []
+    # for col in range(num_cols):
+    #     A = [d[col] for d in run_data]
+    #     sensor_data.append(A)
 
-    run_data = import_data('data.json')
-    num_cols = len(run_data[0])
-    sensor_data = []
-    for col in range(num_cols):
-        A = [d[col] for d in run_data]
-        sensor_data.append(A)
-
-    sensor_data = sensor_data[3:5]
+    # sensor_data = sensor_data[3:5]
 
     # filter_tuner = KalmanFilterTuner(lqgdBot, sensor_data)
     # filter_tuner.run()

builds.py
@@ -20,18 +20,15 @@ class ModelConstants:
     # we add our Q and R matrices
     # focus on angle, ignore angular velocity
     # it's volatile because of high magnitude eigenvalue
-    Q = np.diag([0.001, 0.001, 1, 100])
-
+    Q = np.diag([1, 1, 1, 1])
     R = np.array([[1]])
+
+    Q_kf = np.diag([1,1,1,1])
+    R_kf = np.diag([0.01,1,10])
 
     Q2V = np.array([[1,0],\
                     [0, 1]])
-
     R2V = np.array([[1]])
-
-    Q_kf = np.diag([1,1,1,10])
-    R_kf = np.diag([0.01,0.05,0.1])
-
     Q_kf2V = np.eye(2)
     R_kf2V = np.eye(2)
 
@@ -39,12 +36,9 @@ class ModelConstants:
 # inherit all the values of ModelConstants
 class ExperimentalConstants(ModelConstants):
 
-    Q2V = np.diag([1, 1])
-    R2V = np.array([[1]])
-    
-    Q_kf2V  = np.diag([1, 1])
+    L = 0.06
+    Q = np.diag([1,1,1,1])
+    R = np.array([[1]])
 
-    R_kf2V = np.diag([0.04, 0.0000026])
-    
-    SC = 0.5 * 0.45 / 0.0325 # this is used to translate horizontal force to speed
-    pass
+    Q_kf = np.diag([1,1,1,1])
+    R_kf = np.diag([0.000004,0.01,0.000003])

imu.py
@@ -118,7 +118,7 @@ def verify_accelerometer(imu):
     data = []
     for _ in range(timespan * 100):
         imu.raw_accel_data()
-        angle = math.degrees(imu.raw_angle_rad() - math.pi) 
+        angle = imu.raw_angle_rad() 
         data.append([imu.ay_raw, imu.az_raw, angle])
         sleep(0.01)
 
@@ -130,6 +130,7 @@ def verify_accelerometer(imu):
     print("average accelerometer Z: ", np.average([abs(a[1]) for a in data]))
     print('\n')
 
+    print('readings in radians')
     print("accelerometer angle variance", np.var([(a[2]) for a in data]))
     print("average angle: ", np.average([abs(a[2]) for a in data]))
     print('\n')
@@ -150,6 +151,7 @@ def verify_gyro(imu):
     while timer.running: # wait for the trial to end
         sleep(5)
 
+    print('gyro readings in rad/s')
     print("gyro noise variance", np.var([a[0] for a in data]))
     print("average error gyro error: ", np.average([abs(a[0]) for a in data]))
     print('\n')