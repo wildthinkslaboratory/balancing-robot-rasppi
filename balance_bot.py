I think the angular velocity may have the wrong sign

balance_bot.py
@@ -110,13 +110,7 @@ if __name__ == "__main__":
     # simulator = Simulator(lqgBot, x0, u0, sim_length)
     # simulator.run()
 
-    # simulator = Simulator(lqgdBot, x0, u0, sim_length)
-    # simulator.run()
-
     variances = np.array([0.004,0.00015,0.0000025])
-    # variances = np.array([0.004,0.0015,0.025])
-    # simulator = NoisySimulator(lqgBot, x0, u0, sim_length, noise=variances, nudge=0.0)
-    # simulator.run()
 
     simulator = NoisySimulator(lqgdBot, x0, u0, sim_length, noise=variances, nudge=0.0)
     simulator.run()
@@ -124,7 +118,7 @@ if __name__ == "__main__":
     run_data = import_data('data.json')
     num_cols = len(run_data[0])
     sensor_i = [5,6,7]
-    # run_data = run_data[:300]
+    run_data = run_data[:30]
     sensor_data = np.empty((len(run_data),len(sensor_i)))
     for j, row in enumerate(run_data):
         record = [row[i] for i in sensor_i]

plotRun.py
@@ -1,6 +1,7 @@
 from utilities import import_data
 import sys
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 data = import_data('data.json')
@@ -18,7 +19,7 @@ plt.rcParams.update({
 })
 
 ns=4
-num_points = 100
+num_points = 40
 if num_points > len(data):
      num_points = len(data)
 
@@ -35,7 +36,11 @@ plt.show()
 nu = 1
 fig, axs = plt.subplots(ns + nu)
 fig.set_figheight(8)
+y = np.full(num_points, np.pi)
+
 for i in range(ns):
+    if (i == 2):
+        axs[i].plot(y)
     axs[i].plot(plot_data[i][:num_points],linewidth=2)
     axs[i].set_ylabel(state_names[i])
 for i in range(nu):