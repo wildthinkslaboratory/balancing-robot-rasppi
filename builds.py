I believe I fixed a sign error in the equations

builds.py
@@ -64,7 +64,7 @@ class SegwayConstants:
         self.r = 0.0325               # wheel radius m
         self.M = 0.25435            # mass of body kg
         self.m = 0.757536             # mass of wheels kg
-        self.L = 0.085                 # length from COM body and wheel axis m
+        self.L = 0.08495273492                 # length from COM body and wheel axis m
         self.I_b =  0.008553260629             # inertia of body kg m^2
         self.I_w = 0.000062147      # inertia of wheel kg m^2
         self.ST = 0.45               # stall torque
@@ -74,8 +74,8 @@ class SegwayConstants:
         self.Q = np.diag([1,1,100,10])
         self.R = np.diag([0.1])
 
-        self.Q_kf = np.diag([1,1,1,1]) / 1000
-        self.R_kf = np.diag([0.004,0.00015,0.0000025])
+        self.Q_kf = np.diag([10,1,1,1]) / 100000
+        self.R_kf = np.diag([0.0004,0.00015,0.0000025])
 
         # this stuff all helps with making run data plots
         # I made latex names for my states. They look nice in the simulation plots

segway.py
@@ -55,7 +55,7 @@ s_3 = I_w / r**2 + M + M
 
 T = (0.821 * u + 0.00439) * 9.81 * 0.09
 
-t_1 = -T + g * s_2 * sin(theta)
+t_1 = T - g * s_2 * sin(theta)
 t_2 = T / r + s_2 * thetadot**2 * sin(theta)
 
 # Determinant of our matrix M(theta)
@@ -70,7 +70,8 @@ RHS = ca.vertcat(
 
 
 # set up the K matrix
-goal_state = [0.0, 0.0, sc.balance_point, 0.0]
+# goal_state = [0.0, 0.0, sc.balance_point, 0.0]
+goal_state = [0.0, 0.0, 0.0, 0.0]
 goal_u = [0.0]
 
 # If we want a Kalman Filter we need to pass in a measurement model
@@ -117,7 +118,7 @@ print(lqgBot)
 if __name__ == "__main__":
     # now we can rum a simulation
     u0 = np.array([0.0])
-    x0 = np.array([-1.0,0, 0.0, 0.0]) # Initial condition
+    x0 = np.array([0.0, 0.0, 0.02, 0.0]) # Initial condition
     sim_length = 10 # in seconds
 
     # simulator = Simulator(lqrBot, x0, u0, sim_length)
@@ -126,7 +127,7 @@ if __name__ == "__main__":
     simulator = Simulator(lqgdBot, x0, u0, sim_length)
     simulator.run()
 
-    variances = np.array([0.004, 0.000015,0.0000025])
+    variances = np.array([0.04, 0.000015,0.0000025])
     simulator = NoisySimulator(lqgdBot, x0, u0, sim_length, noise=variances, nudge=0.0)
     simulator.run()
 